8cbdd00b3de1bd92316e57f1479529162041bc9c
HenriqueHCamara <henrique.hoffman@sempreceub.com>
2020-02-13 20:48:13 -0200

    Scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :events
+  resources :reports
+  resources :comments
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#home'
